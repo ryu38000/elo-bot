3c87c2f810cf3f85b5f0555eb3a77ce28a3858fb
ryu38000 <ryu38000@users.noreply.github.com>
2017-10-06 19:02:52 +0200

    Add files via upload

--- src/message.js
+++ src/message.js
@@ -27,9 +27,9 @@ const replyMessage = (message) => {
     * Or: Update your mongo DB
     * etc...
     */
-      var stringfr = 'Très bien si tu veux de la documentation concernant le service ';
-      var stringEn = 'Ok, if you want to have information about ';
-      var stringIt = 'Se vuoi avere informazione a proposto del servizio ';
+      //var stringfr = 'Très bien si tu veux de la documentation concernant le service ';
+      //var stringEn = 'Ok, if you want to have information about ';
+      //var stringIt = 'Se vuoi avere informazione a proposto del servizio ';
 
 
     if (result.action) {
@@ -38,27 +38,28 @@ const replyMessage = (message) => {
 
     if(result.action.slug == "information"){
 
-
       for(var t in result.entities){
-        var string ="";
+        var string ="Très bien si tu veux de la documentation concernant le service ";
+        var stringMail =""
               if(t.match(/produit-elo-sem/i)){
 
                 
-                if(result.language === 'fr'){
-                  string = stringfr + "sémantique alors visite cette page : ";
+               /* if(result.language === 'fr'){
+                  //string = stringfr + "sémantique alors visite cette page : ";
                 }
                 else if (result.language === 'it'){
-                  string = stringIt + "semantico alora fai clic qui : ";
+                  //string = stringIt + "semantico alora fai clic qui : ";
                 }
                 else{
-                  string = stringEn + "semantic visit this website : ";
-                }
+                  //string = stringEn + "semantic visit this website : ";
+                }*/
+                string += "sémantique alors visite ce site: https://www.eloquant.com/la-semantique/explore-analyse-semantique-automatisee";
+                stringMail += "Ou alors envoyer un mail à : support-semantique@eloquant.com";
 
-                string += "https://www.eloquant.com/la-semantique/explore-analyse-semantique-automatisee";
               }
 
                else if(t.match(/produit-elo-dial/i)){
-                  if(result.language === 'fr'){
+                  /*if(result.language === 'fr'){
                     string = stringfr + "dialogue alors visite cette page : ";
                   }
                   else if (result.language === 'it'){
@@ -66,15 +67,18 @@ const replyMessage = (message) => {
                   }
                   else{
                     string = stringEn + "dialog visit this website : ";
-                  }
+                  }*/
 
-                  string += "https://www.eloquant.com/le-dialogue/contact-gestion-des-interactions-clients-multicanal";
+                  string += "dialogue alors visite ce site : https://www.eloquant.com/le-dialogue/contact-gestion-des-interactions-clients-multicanal";
+                  stringMail += "Ou alors envoyer un mail à : support-dialogue@eloquant.com";
+
                 }
+
              else if(t.match(/produit-elo-ec/i)){
 
-              console.log("j'entre ici !")
+              //console.log("j'entre ici !")
 
-                if(result.language === 'fr'){
+               /* if(result.language === 'fr'){
                   string = stringfr + "écoute alors visite cette page : ";
                 }
                 else if (result.language === 'it'){
@@ -82,17 +86,49 @@ const replyMessage = (message) => {
                 }
                 else{
                   string = stringEn + "écoute visit this website : ";
-                }
+                }*/
 
-                string += "https://www.eloquant.com/l-ecoute/interview-solution-de-recueil-de-feedback-multicanal";
+                string += "écoute alors visite ce site : https://www.eloquant.com/l-ecoute/interview-solution-de-recueil-de-feedback-multicanal";
+                stringMail += "Ou alors envoyer un mail à : support-ecoute@eloquant.com";
+
               }
+
+             else if(t.match(/produit-elo-pil/i)){
+
+              //console.log("j'entre ici !")
+
+               /* if(result.language === 'fr'){
+                  string = stringfr + "écoute alors visite cette page : ";
+                }
+                else if (result.language === 'it'){
+                  string = stringIt + "écoute alora fai clic qui : ";
+                }
+                else{
+                  string = stringEn + "écoute visit this website : ";
+                }*/
+
+                string += "écoute alors visite ce site : https://www.eloquant.com/le-pilotage/app-cockpit-pilotage-de-l-experience-client-depuis-un-smartphone";
+                  stringMail += "Ou alors envoyer un mail à : support-cockpit@eloquant.com";
+
+              }
+
+              else{
+                   message.addReply([{ type: 'text', content: "Très bien, vous souhaitez être documenté, quel service vous intéresse? (Ecoute, dialogue, sémantique, pilotage)" }])
+              }
+
                 // prod = true;
                 // nameEntities.push(t);
                 // for(var i = 0 ; i<result.entities[t].length;i++){
                 //   neValue.push(result.entities[t][i].raw)
                 // }
       }
-      if(string===''){
+
+
+      if(stringMail!=''){
+        message.addReply([{ type: 'text', content: string },{ type: 'text', content: stringMail },{ type: 'text', content: "De quoi d'autre souhaiteriez vous discuter ?" }])
+      }
+
+      /*if(string===''){
           if(result.language === 'fr'){
               string = 'De quoi voulez-vous parler ?';
           }
@@ -103,17 +139,17 @@ const replyMessage = (message) => {
              string ='What do you want to talk about?';
           }
          message.addReply([{ type: 'text', content: string }])        
-      }
-      else{
-                  message.addReply([{ type: 'text', content: string },{ type: 'text', content: "Avez-vous d'autres questions ?" }])
-      }
+      }*/
+      //else{
+      //}
     }
 
 
         if(result.action.slug == "exportation"){
                     console.log("exportation")
+          //message.addReply([{ type: 'text', content: "Avez-vous d'autres questions ?" }])
 
-          message.addReply([{ type: 'text', content: "Voici un lien vers la documentation : https://www.eloquant.com/pages/les-manuels-utilisateurs" },{ type: 'text', content: "Avez-vous d'autres questions ?" }])
+          //message.addReply([{ type: 'text', content: "Voici un lien vers la documentation : https://www.eloquant.com/pages/les-manuels-utilisateurs" },{ type: 'text', content: "Avez-vous d'autres questions ?" }])
         }
 
 
@@ -132,7 +168,6 @@ const replyMessage = (message) => {
               ]
             }
           }
-
 
           message.addReply(call)
         }
